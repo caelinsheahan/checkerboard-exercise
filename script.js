a86c0e7f45aca18c2e282f0f83b9d9e49962c338
Caelin Sheahan <caelinsheahan@gmail.com>
2017-10-13 15:33:01 -0600

    normal checkers

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
-// Your JS goes here
+document.addEventListener("DOMContentLoaded", function(event) {
+  var head = document.head
+  var html = document.getElementsByTagName('html')[0];
+  var body = document.body;
+body.innerHTML =
+`<div id="container">
+  <div class="parent">
+    <div class="childb"></div>
+    <div class="childr"></div>
+    <div class="childb"></div>
+    <div class="childr"></div>
+  </div>
+  <div class="parent">
+    <div class="childr"></div>
+    <div class="childb"></div>
+    <div class="childr"></div>
+    <div class="childb"></div>
+  </div>
+  <div class="parent">
+    <div class="childb"></div>
+    <div class="childr"></div>
+    <div class="childb"></div>
+    <div class="childr"></div>
+  </div>
+    <div class="parent">
+    <div class="childr"></div>
+    <div class="childb"></div>
+    <div class="childr"></div>
+    <div class="childb"></div>
+  </div>
+</div>`
+
+  var styles = document.createElement('style');
+  head.appendChild(styles)
+  styles.innerHTML = `* {
+  margin: 0;
+  padding: 0;
+}
+
+body, html {
+  height: 100%;
+}
+
+#container {
+  height: 100%
+}
+
+.parent {
+  display: flex;
+  height: 25%;
+}
+
+.childb {
+  width: 33.33%;
+  height: 100%;
+  margin: auto;
+}
+
+.childr {
+  width: 33.33%;
+  height: 100%;
+  margin: auto;
+}
+
+.childb {
+  background: black
+}
+.childr {
+  background: red
+}
+
+`
+
+})
